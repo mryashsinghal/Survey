854ebe12b977df0e611d9ad2e2d0400484299cc3
Yash Singhal <152426138+mryashsinghal@users.noreply.github.com>
2024-09-23 14:44:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,23 @@ const  fs  = require('fs');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+const Ans = require('./model/ans.js');
+const dbConnection = require('./config/db.js')
 app.use(cors());
 app.use(express.json());
-
-app.post('/',(req,res)=>{
-   let data = req.body;
+require('dotenv').config();
+dbConnection();
+ 
+app.post('/', async (req,res)=>{
+   let data = req.body.ans;
    console.log(data);
    //here the answer is saved to the database safely
 
+   let a  = new Ans({data});
+   await a.save();
+   console.log(a);
+
+ 
 })
 
 app.listen(5000);
